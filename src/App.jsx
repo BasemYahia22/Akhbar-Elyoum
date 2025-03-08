b122b4158831fdae35f78990182d9ac21e9c66cc
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-08 22:21:21 +0200

    feat: Add ThemeProvider and configure routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import Login from "./pages/Login"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext"; // Import ThemeProvider
+import StudentDashboard from "./pages/StudentDashboard";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
-    <>
-    <Login/>
-    </>
-  )
+    // Wrap the entire app with ThemeProvider
+    <ThemeProvider>
+      <Routes>
+        {/* Login Route */}
+        <Route path="/" element={<Login />} />
+
+        {/* Student Dashboard Route */}
+        <Route path="/student/*" element={<StudentDashboard />} />
+
+        {/* Teacher Dashboard Route (Commented Out) */}
+        {/* <Route path="/teacher" element={<TeacherDashboard />} /> */}
+
+        {/* Admin Dashboard Route (Commented Out) */}
+        {/* <Route path="/admin" element={<AdminDashboard />} /> */}
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
