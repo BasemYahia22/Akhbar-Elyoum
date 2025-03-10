abbb36c1cad6ed957cc813faed6ab133d069f444
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-10 16:33:11 +0200

    feat: Add ProfessorDataSection component

---
+++ src/components/ProfessorDataSection.jsx
@@ -0,0 +1,20 @@
+import professorImg from "../assets/Professor.png";
+
+const ProfessorDataSection = () => {
+  return (
+    <div className="flex items-center justify-between text-white rounded-lg bg-primary">
+      <div className="p-3 md:p-8 lg:w-2/3">
+        <h1 className="text-2xl md:text-3xl font-crimson-text-semibold">
+          Welcome back, Smith Johnson
+        </h1>
+        <p className="text-gray-300 md:text-lg font-crimson-text-regular">
+          You have 27 new student added to your domain. Please reach out to the
+          Head Teaching Assistant if you want them excluded from your domain.
+        </p>
+      </div>
+      <img src={professorImg} alt="professorImg" className="hidden md:block" />
+    </div>
+  );
+};
+
+export default ProfessorDataSection;
