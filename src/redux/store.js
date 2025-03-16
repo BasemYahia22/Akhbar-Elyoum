bc16757f115a4422f3f224859a31a59386a47149
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-17 00:11:35 +0200

    feat(store):Add studentHomepageSlice and Initialize state from local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,26 @@ import authReducer from "./slices/authSlice";
 import studentReducer from "./slices/studentSlice";
 import profReducer from "./slices/profSlice";
 import adminReducer from "./slices/adminSlice";
+import studentHomepageReducer from "./slices/studentHomepageSlice";
+
+// Initialize state from local storage
+const preloadedState = {
+  auth: {
+    role: localStorage.getItem("role") || null,
+    isAuthenticated:
+      localStorage.getItem("isAuthenticated") === "true" || false,
+    loading: false,
+    error: null,
+  },
+};
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    studentHomepage: studentHomepageReducer,
     student: studentReducer,
     prof: profReducer,
     admin: adminReducer,
   },
+  preloadedState,
 });
