a6d3e55925c1394c957dea7e3ffc6f027bc70780
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-17 22:27:32 +0200

    refactor(Dashboard): Extract Statistics and Notifications to new files

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,20 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBook,
-  faUserGraduate,
-  faChalkboardTeacher,
-  faBell,
-  faClock,
-  faChartLine,
-} from "@fortawesome/free-solid-svg-icons";
+import { faClock, faChartLine } from "@fortawesome/free-solid-svg-icons";
 import ProfessorAndAdminDataSection from "../../components/ProfessorAndAdminDataSection";
+import Statistics from "../../components/Statistics";
+import LastNotifications from "../../components/LastNotifications";
 
 const Dashboard = () => {
-  // Styles
-  const statsContainerStyle1 =
-    "p-6 bg-white rounded-lg shadow text-center h-fit";
+  // Styles;
   const h2Style = "text-xl font-semibold";
-  const pStyle = "text-3xl font-bold border-b-2 w-fit mx-auto pb-1";
   const liStyle = "flex items-center";
 
   // Mock data
@@ -54,38 +46,7 @@ const Dashboard = () => {
         {/* Statistics and table */}
         <div className="col-span-2">
           {/* Statistics */}
-          <div className="grid grid-cols-1 col-span-2 gap-6 md:grid-cols-3 h-fit">
-            <div className={statsContainerStyle1}>
-              <div>
-                <FontAwesomeIcon
-                  icon={faBook}
-                  className="text-3xl text-blue-500 "
-                />
-                <h2 className={h2Style}>Courses</h2>
-              </div>
-              <p className={`${pStyle} border-blue-500`}>56</p>
-            </div>
-            <div className={statsContainerStyle1}>
-              <div>
-                <FontAwesomeIcon
-                  icon={faUserGraduate}
-                  className="text-3xl text-green-500 "
-                />
-                <h2 className={h2Style}>Students</h2>
-              </div>
-              <p className={`${pStyle} border-green-500`}>1234</p>
-            </div>
-            <div className={statsContainerStyle1}>
-              <div>
-                <FontAwesomeIcon
-                  icon={faChalkboardTeacher}
-                  className="text-3xl text-yellow-500 "
-                />
-                <h2 className={h2Style}>Classes</h2>
-              </div>
-              <p className={`${pStyle} border-yellow-500`}>45</p>
-            </div>
-          </div>
+          <Statistics />
 
           {/* Top Students Table */}
           <div className="p-3 mt-5 bg-white rounded-lg shadow md:p-6">
@@ -118,27 +79,7 @@ const Dashboard = () => {
         {/* Notifications and Recent Activities */}
         <div>
           {/* Notifications */}
-          <div className="p-6 bg-white rounded-lg shadow">
-            <div className="flex items-center mb-4">
-              <FontAwesomeIcon
-                icon={faBell}
-                className="mr-4 text-3xl text-blue-500"
-              />
-              <h2 className={h2Style}>Latest Notifications</h2>
-            </div>
-            <ul className="space-y-2">
-              {latestNotifications.map((notification) => (
-                <li key={notification.id} className={liStyle}>
-                  <FontAwesomeIcon
-                    icon={faBell}
-                    className="mr-2 text-blue-500"
-                  />
-                  {notification.message}
-                </li>
-              ))}
-            </ul>
-          </div>
-
+          <LastNotifications latestNotifications={latestNotifications} />
           {/* Recent Activities */}
           <div className="p-6 mt-5 bg-white rounded-lg shadow">
             <div className="flex items-center mb-4">
