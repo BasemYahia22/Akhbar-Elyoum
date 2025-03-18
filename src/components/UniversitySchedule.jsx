df968e0aa7c4d57962ccc8c48b64ade38eb1f78b
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-18 12:56:01 +0200

    refactor(UniversitySchedule): Exract university info to new component

--- src/components/UniversitySchedule.jsx
+++ src/components/UniversitySchedule.jsx
@@ -1,15 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarAlt, faDownload } from "@fortawesome/free-solid-svg-icons"; // Import FontAwesome icons
+import UniversityInfo from "./UniversityInfo";
 
 const UniversitySchedule = () => {
   const iconStyle = "p-3 text-2xl text-white rounded-lg bg-primary";
   const dateStyle = "text-sm text-gray-500";
   const containerStyle = "max-w-3xl p-6 mx-auto bg-white rounded-lg shadow-md";
   const scheduleH2Style = "text-sm font-semibold text-gray-800";
-  const aboutH2Style = "mb-2 text-xl text-gray-800 font-crimson-text-bold";
-  const aboutTextStyle = "text-[16px] text-gray-500 font-crimson-text-regular";
-  const buttonStyle =
-    "py-2 text-lg transition text-primary font-crimson-text-bold";
 
   // Replace these with the actual paths to your PDF files
   const lecturesSchedulePdf = "/path/to/lectures-schedule.pdf";
@@ -60,26 +57,7 @@ const UniversitySchedule = () => {
           </div>
         </div>
       </div>
-      <div className={containerStyle}>
-        {/* About University Section */}
-        <div className="mb-8">
-          <h2 className={aboutH2Style}>About University</h2>
-          <p className={aboutTextStyle}>
-            Sorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-          <button className={buttonStyle}>See More</button>
-        </div>
-
-        {/* About Credit Hours Section */}
-        <div>
-          <h2 className={aboutH2Style}>About Credit Hours</h2>
-          <p className={aboutTextStyle}>
-            Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            vulputate libero et velit interdum.
-          </p>
-          <button className={buttonStyle}>See More</button>
-        </div>
-      </div>
+      <UniversityInfo />
     </div>
   );
 };
