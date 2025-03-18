77ed5506882ff35c3b7a264bdfe82615c5fe243d
BasemYahia22 <basemyahiagithup@gmail.com>
2025-03-18 12:58:41 +0200

    feat: Create ShowStudentGrades  page

---
+++ src/pages/professor/ShowStudentGrades.jsx
@@ -0,0 +1,11 @@
+import StudentTable from '../../components/StudentTable';
+
+const ShowStudentGrades = () => {
+    return (
+      <div>
+        <StudentTable />
+      </div>
+    );
+}
+
+export default ShowStudentGrades;
