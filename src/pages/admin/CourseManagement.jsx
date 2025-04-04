b547c96bec401a593deb82254e2d5e56beb4758f
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-04 22:52:03 +0200

    feat: fetch data from backend API in CourseManagement

--- src/pages/admin/CourseManagement.jsx
+++ src/pages/admin/CourseManagement.jsx
@@ -1,131 +1,115 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEdit,
-  faTrash,
   faPlus,
   faEye,
   faEyeSlash,
+  faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import CreateCourseModal from "./CreateCourseModal";
-import courseImage from "../../assets/courseImg.png";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCourses } from "../../redux/slices/fetchCoursesSlice";
+import { toggleCourseStatus } from "../../redux/slices/toggleCourseStatusSlice";
+import Message from "../../components/Message";
+import { addAndUpdateCourse } from "../../redux/slices/addAndUpdateCourseSlice";
+import StatusMessage from "../../components/StatusMessage";
+import { fetchProfessorInfo } from "../../redux/slices/fetchProfessorInfoSlice";
 const CourseManagement = () => {
-  const [courses, setCourses] = useState([
-    {
-      id: 1,
-      name: "Introduction to Computer Science",
-      courseCode: "CS101",
-      instructor: "Dr. John Doe",
-      semester: "Fall",
-      year: 2023,
-      img: courseImage,
-      courseHours: 3,
-      isActive: true,
-    },
-    {
-      id: 2,
-      name: "Advanced Mathematics",
-      courseCode: "MATH202",
-      instructor: "Dr. Jane Smith",
-      semester: "Spring",
-      year: 2024,
-      img: courseImage,
-      courseHours: 4,
-      isActive: true,
-    },
-  ]);
-
-  const [filters, setFilters] = useState({
-    semester: "",
-    year: "",
-    instructor: "",
-  });
-  const inputStyle = "p-2 border rounded-lg focus:outline-none";
-
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const { data, loading, error } = useSelector((state) => state.fetchCourses);
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputStyle =
+    "p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500";
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedCourse, setSelectedCourse] = useState(null);
 
   // Add or update a course
-  const handleSubmit = (course) => {
-    if (selectedCourse) {
-      setCourses(
-        courses.map((c) =>
-          c.id === selectedCourse.id ? { ...course, id: c.id } : c
-        )
-      );
+  const handleSubmit = (operation, credentials) => {
+    if (operation === "add") {
+      dispatch(addAndUpdateCourse({ operation, credentials }))
+        .unwrap()
+        .then(() => {
+          showMessage("added course Successfully!", "success");
+          dispatch(fetchCourses());
+        })
+        .catch((error) => {
+          showMessage(error, "error");
+        });
     } else {
-      setCourses([...courses, { ...course, id: courses.length + 1 }]);
+      dispatch(addAndUpdateCourse({ operation, credentials }))
+        .unwrap()
+        .then(() => {
+          showMessage("updated course Successfully!", "success");
+          dispatch(fetchCourses());
+        })
+        .catch((error) => {
+          showMessage(error, "error");
+        });
     }
     setIsModalOpen(false);
     setSelectedCourse(null);
   };
 
-  // Delete a course
-  const deleteCourse = (id) => {
-    setCourses(courses.filter((course) => course.id !== id));
-  };
-
   // Toggle active state of a course
-  const toggleCourseActive = (id) => {
-    setCourses(
-      courses.map((course) =>
-        course.id === id ? { ...course, isActive: !course.isActive } : course
-      )
-    );
+  const toggleCourseActive = (id, course_status) => {
+    const credentials = {
+      course_id: id,
+      course_status: course_status === 1 ? 0 : 1,
+    };
+    console.log(credentials);
+    dispatch(toggleCourseStatus(credentials))
+      .unwrap()
+      .then(() => {
+        showMessage("Your Toggle Submited Successfully!", "success");
+        dispatch(fetchCourses());
+      })
+      .catch((error) => {
+        showMessage(error, "error");
+      });
   };
+  const showMessage = (msg, type) => {
+    setMessage(msg);
+    setMessageType(type);
+  };
+  // Filter courses by name
+  const filteredCourses = data?.course_management_data?.filter((course) =>
+    course.course_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  // Filter courses
-  const filteredCourses = courses.filter((course) => {
-    return (
-      (filters.semester === "" || course.semester === filters.semester) &&
-      (filters.year === "" || course.year.toString() === filters.year) &&
-      (filters.instructor === "" ||
-        course.instructor
-          .toLowerCase()
-          .includes(filters.instructor.toLowerCase()))
-    );
-  });
+  useEffect(() => {
+    if (!data) {
+      dispatch(fetchCourses());
+      dispatch(fetchProfessorInfo());
+    }
+  }, [dispatch, data]);
+  if (loading || error) {
+    return <StatusMessage loading={loading} error={error} />;
+  }
 
   return (
     <div>
       <h1 className="mb-4 text-2xl font-bold">Course Management</h1>
 
-      {/* Filters and Add Button */}
+      {/* Search and Add Button */}
       <div className="flex flex-col items-center justify-between mb-6 space-y-4 md:flex-row md:space-y-0">
-        <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-          <select
-            value={filters.semester}
-            onChange={(e) =>
-              setFilters({ ...filters, semester: e.target.value })
-            }
-            className={inputStyle}
-          >
-            <option value="">All Semesters</option>
-            <option value="Fall">Fall</option>
-            <option value="Spring">Spring</option>
-            <option value="Summer">Summer</option>
-          </select>
-          <input
-            type="number"
-            placeholder="Year"
-            value={filters.year}
-            onChange={(e) => setFilters({ ...filters, year: e.target.value })}
-            className={inputStyle}
-          />
+        <div className="relative w-full md:w-96">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <FontAwesomeIcon icon={faSearch} className="text-gray-400" />
+          </div>
           <input
             type="text"
-            placeholder="Instructor"
-            value={filters.instructor}
-            onChange={(e) =>
-              setFilters({ ...filters, instructor: e.target.value })
-            }
-            className={inputStyle}
+            placeholder="Search by course name..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className={`${inputStyle} w-full pl-10 `}
           />
         </div>
         <button
           onClick={() => setIsModalOpen(true)}
-          className="px-4 py-2 text-white rounded-lg bg-third"
+          className="w-full px-4 py-2 text-white rounded-lg bg-third md:w-fit"
         >
           <FontAwesomeIcon icon={faPlus} className="mr-2" />
           Add Course
@@ -134,86 +118,87 @@ const CourseManagement = () => {
 
       {/* Course Table */}
       <div className="overflow-x-auto bg-white rounded-lg shadow-lg max-w-[18rem] md:max-w-full">
-        {filteredCourses.length === 0 ? (
+        {filteredCourses?.length === 0 ? (
           <div className="p-6 text-center text-gray-500">
-            No courses found. Try adjusting your filters or add a new course.
+            No courses found. Try adjusting your search or add a new course.
           </div>
         ) : (
           <table className="min-w-full divide-y divide-gray-200 table-auto">
             <thead>
               <tr className="bg-gray-100">
-                <th className="p-2">Image</th>
                 <th className="p-2">Name</th>
-                <th className="p-2">Course Code</th>
+                <th className="p-2">Code</th>
                 <th className="p-2">Instructor</th>
+                <th className="p-2">Department</th>
+                <th className="p-2">Credit Hours</th>
                 <th className="p-2">Semester</th>
-                <th className="p-2">Year</th>
-                <th className="p-2">Course Hours</th>
+                <th className="p-2">Squad</th>
                 <th className="p-2">Status</th>
                 <th className="p-2">Actions</th>
               </tr>
             </thead>
             <tbody>
-              {filteredCourses.map((course) => (
-                <tr
-                  key={course.id}
-                  className="text-center border-b hover:bg-gray-50"
-                >
-                  <td className="p-2">
-                    <img
-                      src={course.img}
-                      alt={course.name}
-                      className="w-12 h-12 rounded-lg"
-                    />
-                  </td>
-                  <td className="p-2 break-words">{course.name}</td>
-                  <td className="p-2 break-words">{course.courseCode}</td>
-                  <td className="p-2 break-words">{course.instructor}</td>
-                  <td className="p-2">{course.semester}</td>
-                  <td className="p-2">{course.year}</td>
-                  <td className="p-2">{course.courseHours}</td>
-                  <td className="p-2">
-                    <span
-                      className={`px-2 py-1 text-sm rounded-full ${
-                        course.isActive
-                          ? "bg-green-100 text-green-800"
-                          : "bg-red-100 text-red-800"
-                      }`}
-                    >
-                      {course.isActive ? "Active" : "Inactive"}
-                    </span>
-                  </td>
-                  <td className="flex items-center justify-center gap-2 p-4 md:flex-wrap">
-                    <button
-                      onClick={() => {
-                        setSelectedCourse(course);
-                        setIsModalOpen(true);
-                      }}
-                      className="px-2 py-1 text-white bg-yellow-500 rounded-lg hover:bg-yellow-600"
-                    >
-                      <FontAwesomeIcon icon={faEdit} />
-                    </button>
-                    <button
-                      onClick={() => deleteCourse(course.id)}
-                      className="px-2 py-1 text-white bg-red-500 rounded-lg hover:bg-red-600"
-                    >
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                    <button
-                      onClick={() => toggleCourseActive(course.id)}
-                      className={`px-2 py-1 text-white rounded-lg ${
-                        course.isActive
-                          ? "bg-green-500 hover:bg-green-600"
-                          : "bg-gray-500 hover:bg-gray-600"
-                      }`}
-                    >
-                      <FontAwesomeIcon
-                        icon={course.isActive ? faEye : faEyeSlash}
-                      />
-                    </button>
-                  </td>
-                </tr>
-              ))}
+              {filteredCourses?.map((course) => {
+                const status =
+                  course.course_status === 0 ? "Active" : "Inactive";
+                return (
+                  <tr
+                    key={course.course_id}
+                    className="text-center border-b hover:bg-gray-50"
+                  >
+                    <td className="p-2 break-words">{course.course_name}</td>
+                    <td className="p-2 break-words">{course.course_code}</td>
+                    <td className="p-2 break-words">
+                      {course.prof_info?.prof_name || course.prof_name}
+                    </td>
+                    <td className="p-2 break-words">
+                      {course.prof_info?.prof_department || course.department}
+                    </td>
+                    <td className="p-2">{course.credit_hours}</td>
+                    <td className="p-2">{course.semester_number}</td>
+                    <td className="p-2">{course.squad_number}</td>
+                    <td className="p-2">
+                      <span
+                        className={`px-2 py-1 text-sm rounded-full ${
+                          course.course_status === 0
+                            ? "bg-green-100 text-green-800"
+                            : "bg-red-100 text-red-800"
+                        }`}
+                      >
+                        {status}
+                      </span>
+                    </td>
+                    <td className="flex items-center justify-center gap-2 p-4 md:flex-wrap">
+                      <button
+                        onClick={() => {
+                          setSelectedCourse(course);
+                          setIsModalOpen(true);
+                        }}
+                        className="px-2 py-1 text-white bg-yellow-500 rounded-lg hover:bg-yellow-600"
+                      >
+                        <FontAwesomeIcon icon={faEdit} />
+                      </button>
+                      <button
+                        onClick={() =>
+                          toggleCourseActive(
+                            course.course_id,
+                            course.course_status
+                          )
+                        }
+                        className={`px-2 py-1 text-white rounded-lg ${
+                          course.course_status === 0
+                            ? "bg-green-500 hover:bg-green-600"
+                            : "bg-gray-500 hover:bg-gray-600"
+                        }`}
+                      >
+                        <FontAwesomeIcon
+                          icon={course.course_status === 0 ? faEye : faEyeSlash}
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         )}
@@ -229,6 +214,14 @@ const CourseManagement = () => {
         onSubmit={handleSubmit}
         course={selectedCourse}
       />
+      {/* Message Component for displaying alerts */}
+      {message && (
+        <Message
+          message={message}
+          type={messageType}
+          onClose={() => setMessage("")}
+        />
+      )}
     </div>
   );
 };
