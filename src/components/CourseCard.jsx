fffbcbc20cd229625fddf63b1286b8f52b3a23da
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-04 14:06:07 +0200

    refactor: change names of keys of data object

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,16 +1,23 @@
+import courseImg from "../assets/courseImg.png";
+
+// Card component to display course information with select/cancel options
 const CourseCard = ({ course, isSelected, onSelect, onCancel }) => {
   return (
     <div className="flex flex-col items-center bg-white shadow-lg rounded-xl">
+      {/* Course image */}
       <img
-        src={course.image}
-        alt={course.name}
+        src={courseImg}
+        alt={course.CourseName}
         className="object-cover w-full h-48 rounded-md"
       />
+
+      {/* Course details */}
       <div className="flex flex-col justify-between p-4 text-center">
-        <h3 className="text-lg font-semibold">{course.name}</h3>
-        <p className="text-primary ">Course Code: {course.code}</p>
-        <p className="text-primary ">Hours: {course.hours} Credit Hours</p>
-        <p className=" text-primary">Prerequisites: {course.prerequisites}</p>
+        <h3 className="text-lg font-semibold">{course.CourseName}</h3>
+        <p className="text-primary">Course Code: {course.CourseCode}</p>
+        <p className="text-primary">Hours: {course.CreditHours} Credit Hours</p>
+
+        {/* Toggle between Select/Cancel button */}
         <div className="mt-2">
           {isSelected ? (
             <button
