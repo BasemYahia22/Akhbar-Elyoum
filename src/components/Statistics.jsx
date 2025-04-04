cdd5a101688194ee9e2cb5e3aa58cdc3bc05a622
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-04 15:07:26 +0200

    feat: fetch data from backend API in Statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,24 +1,30 @@
 import {
   faBook,
-  faChalkboardTeacher,
+  faTasks,
   faUserGraduate,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSelector } from "react-redux";
 const Statistics = () => {
   // Styles
   const statsContainerStyle1 =
     "p-6 bg-white rounded-lg shadow text-center h-fit";
   const h2Style = "text-xl font-semibold";
   const pStyle = "text-3xl font-bold border-b-2 w-fit mx-auto pb-1";
+  const { data } = useSelector((state) => state.profDashboard);
   return (
-    <div className="grid grid-cols-1 col-span-2 gap-6 md:grid-cols-3 h-fit">
+    <div className="grid grid-cols-1 col-span-3 gap-6 md:grid-cols-4 h-fit">
+      // courses numbers
       <div className={statsContainerStyle1}>
         <div>
           <FontAwesomeIcon icon={faBook} className="text-3xl text-blue-500 " />
           <h2 className={h2Style}>Courses</h2>
         </div>
-        <p className={`${pStyle} border-blue-500`}>56</p>
+        <p className={`${pStyle} border-blue-500`}>
+          {data?.courses.Courses_Number}
+        </p>
       </div>
+      // total student
       <div className={statsContainerStyle1}>
         <div>
           <FontAwesomeIcon
@@ -27,17 +33,35 @@ const Statistics = () => {
           />
           <h2 className={h2Style}>Students</h2>
         </div>
-        <p className={`${pStyle} border-green-500`}>1234</p>
+        <p className={`${pStyle} border-green-500`}>
+          {data?.number_students.total_number_students}
+        </p>
       </div>
+      // count assigned tasks
       <div className={statsContainerStyle1}>
         <div>
           <FontAwesomeIcon
-            icon={faChalkboardTeacher}
-            className="text-3xl text-yellow-500 "
+            icon={faTasks}
+            className="text-3xl text-yellow-500"
           />
-          <h2 className={h2Style}>Classes</h2>
+          <h2 className={h2Style}>Assignments</h2>
         </div>
-        <p className={`${pStyle} border-yellow-500`}>45</p>
+        <p className={`${pStyle} border-yellow-500`}>
+          {data?.assigned_tasks?.assigned_tasks}
+        </p>
+      </div>
+      // count submited tasks
+      <div className={statsContainerStyle1}>
+        <div>
+          <FontAwesomeIcon
+            icon={faTasks}
+            className="text-3xl text-yellow-500"
+          />
+          <h2 className={h2Style}>Assignments submit</h2>
+        </div>
+        <p className={`${pStyle} border-yellow-500`}>
+          {data?.submited_tasks?.submited_tasks}
+        </p>
       </div>
     </div>
   );
