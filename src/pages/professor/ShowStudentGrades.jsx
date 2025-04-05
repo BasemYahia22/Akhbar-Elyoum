8474b82d47247ada76bc11c4e2565233ce77db77
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 18:43:42 +0200

    feat: fetch data from backend API in StudentAssignment

--- src/pages/professor/ShowStudentGrades.jsx
+++ src/pages/professor/ShowStudentGrades.jsx
@@ -1,11 +1,29 @@
-import StudentTable from '../../components/StudentTable';
+import { useEffect } from "react";
+import StudentTable from "../../components/StudentTable";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStudentGrades } from "../../redux/slices/fetchStudentGradesSlice";
+import StatusMessage from "../../components/StatusMessage";
 
 const ShowStudentGrades = () => {
-    return (
-      <div>
-        <StudentTable />
-      </div>
-    );
-}
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector(
+    (state) => state.fetchStudentGrades
+  );
+  const { role } = useSelector((state) => state.auth);
+  // Fetch student grades data on component mount
+  useEffect(() => {
+    if (!data) {
+      dispatch(fetchStudentGrades(role));
+    }
+  }, [dispatch, data]);
+ if (loading || error) {
+   return <StatusMessage loading={loading} error={error} />;
+ }
+  return (
+    <div>
+      <StudentTable data={data} />
+    </div>
+  );
+};
 
 export default ShowStudentGrades;
