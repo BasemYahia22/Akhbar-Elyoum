f6ee640226ad94cb70abda3c09e813759d9e623e
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 17:22:14 +0200

    feat: fetch data from backend API in Dashboard

--- src/pages/student/Dashboard.jsx
+++ src/pages/student/Dashboard.jsx
@@ -1,11 +1,26 @@
-import React from "react";
 import UserDataSection from "../../components/UserDataSection";
 import FilterSection from "../../components/FilterSection";
 import SemesterGrades from "../../components/SemesterGrades";
 import GradeGuide from "../../components/GradeGuide";
 import UniversitySchedule from "../../components/UniversitySchedule";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchStudentHomepage } from "../../redux/slices/studentHomepageSlice";
+import StatusMessage from "../../components/StatusMessage";
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector(
+    (state) => state.studentHomepage
+  );
+  // Fetch student homepage data on component mount
+  useEffect(() => {
+    if (!data) {
+      dispatch(fetchStudentHomepage());
+    }
+  }, [dispatch, data]);
+  if (loading || error) {
+    return <StatusMessage loading={loading} error={error} />;
+  }
   return (
     <div>
       <UserDataSection />
