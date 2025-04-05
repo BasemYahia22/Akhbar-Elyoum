bcc48f1c7cacaf93c8b8cd7661e5dd90e61ac76e
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 19:34:06 +0200

    refactor: Update call store and persistor

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
-);
+import { createStore } from "./redux/store.js";
+
+(async () => {
+  const { store, persistor } = await createStore();
+
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+})();
