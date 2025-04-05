c176c4db8f9e7b33759a9c6af9d3c348a6330608
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 11:37:45 +0200

    refactor: remove repeated style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,67 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faTelegram } from "@fortawesome/free-brands-svg-icons"; // Brand icons
-import { faMapMarkerAlt, faPhoneAlt } from "@fortawesome/free-solid-svg-icons"; // Solid icons
+import { faLinkedin, faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faMapMarkerAlt, faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext"; // Import ThemeContext
+import { ThemeContext } from "../context/ThemeContext";
 
 const Footer = () => {
-  const { isDarkMode } = useContext(ThemeContext); // Use ThemeContext
+  // Get current theme mode
+  const { isDarkMode } = useContext(ThemeContext);
 
+  // Style constants
   const socialLinkStyle = "transition duration-300";
+  const footerBgStyle = isDarkMode ? "bg-gray-800" : "bg-primary";
+  const socialIconColor = isDarkMode ? "text-gray-300" : "text-white";
+
   return (
-    <footer
-      className={`p-5 text-center ${
-        isDarkMode ? "text-white bg-gray-800" : "text-white bg-primary"
-      }`}
-    >
+    <footer className={`p-5 text-center text-white ${footerBgStyle}`}>
+      {/* Footer links and visit counter */}
       <p>
         Terms of Use | Privacy Policy | FAQ | Customer Service Character |
         Number of Visits: 134565
       </p>
-      {/* Flex container for copyright and social links */}
+
+      {/* Footer bottom section - copyright and social links */}
       <div className="flex flex-col items-center justify-between md:flex-row">
-        {/* Copyright Text - Centered */}
+        {/* Copyright text */}
         <p className="flex-1 text-center font-crimson-text-regular">
           ©Akhbar Elyoum Academy 2020-2025
         </p>
-        {/* Social Links - Aligned to the Right */}
+
+        {/* Social media and contact links */}
         <div className="flex space-x-4">
+          {/* LinkedIn */}
           <a
             href="https://www.linkedin.com"
             target="_blank"
             rel="noopener noreferrer"
-            className={`${socialLinkStyle} ${
-              isDarkMode
-                ? "text-gray-300 hover:text-blue-400"
-                : "text-white hover:text-blue-400"
-            }`}
+            className={`${socialLinkStyle} ${socialIconColor} hover:text-blue-400`}
           >
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
+
+          {/* Telegram */}
           <a
             href="https://telegram.org"
             target="_blank"
             rel="noopener noreferrer"
-            className={`${socialLinkStyle} ${
-              isDarkMode
-                ? "text-gray-300 hover:text-blue-500"
-                : "text-white hover:text-blue-500"
-            }`}
+            className={`${socialLinkStyle} ${socialIconColor} hover:text-blue-500`}
           >
             <FontAwesomeIcon icon={faTelegram} />
           </a>
+
+          {/* Location */}
           <a
             href="#"
-            className={`${socialLinkStyle} ${
-              isDarkMode
-                ? "text-gray-300 hover:text-green-500"
-                : "text-white hover:text-green-500"
-            }`}
+            className={`${socialLinkStyle} ${socialIconColor} hover:text-green-500`}
           >
             <FontAwesomeIcon icon={faMapMarkerAlt} />
           </a>
+          
+          {/* Phone */}
           <a
             href="tel:+1234567890"
-            className={`${socialLinkStyle} ${
-              isDarkMode
-                ? "text-gray-300 hover:text-green-500"
-                : "text-white hover:text-green-500"
-            }`}
+            className={`${socialLinkStyle} ${socialIconColor} hover:text-green-500`}
           >
             <FontAwesomeIcon icon={faPhoneAlt} />
           </a>
