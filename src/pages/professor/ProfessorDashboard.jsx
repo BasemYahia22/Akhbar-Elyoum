8dbfe4a3ddb506f351bd5857b7960b4d99738c4a
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 18:44:30 +0200

    refactor: remove NotificationsProvider

--- src/pages/professor/ProfessorDashboard.jsx
+++ src/pages/professor/ProfessorDashboard.jsx
@@ -5,7 +5,6 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { ThemeContext } from "../../context/ThemeContext";
 import Notifications from "../Notifications";
-import { NotificationsProvider } from "../../context/NotificationsContext";
 import Dashboard from "./Dashboard";
 import Assignment from "./Assignment";
 import StudentGrades from "./StudentGrades";
@@ -19,61 +18,59 @@ const ProfessorDashboard = () => {
   };
   return (
     <div>
-      <NotificationsProvider>
-        <div className="flex min-h-screen">
-          {/* Sidebar (Fixed) */}
-          <Sidebar
-            isOpen={isSidebarOpen}
-            toggleSidebar={toggleSidebar}
-            userRole="professor"
-          />
+      <div className="flex min-h-screen">
+        {/* Sidebar (Fixed) */}
+        <Sidebar
+          isOpen={isSidebarOpen}
+          toggleSidebar={toggleSidebar}
+          userRole="professor"
+        />
 
-          {/* Main Content */}
-          <div className="flex flex-col flex-1 min-h-screen lg:ml-[255px]">
-            <Navbar toggleSidebar={toggleSidebar} userRole="professor" />
+        {/* Main Content */}
+        <div className="flex flex-col flex-1 min-h-screen lg:ml-[255px]">
+          <Navbar toggleSidebar={toggleSidebar} userRole="professor" />
 
-            {/* Main Content Wrapper (Scrollable) */}
-            <div className="flex flex-col flex-grow overflow-y-auto mt-[136px] md:mt-[76px]">
-              <main
-                className={`flex-grow p-4 lg:p-8 ${
-                  isDarkMode
-                    ? "bg-gray-900 text-white"
-                    : "bg-gray-100 text-gray-700"
-                }`}
-              >
-                <Routes>
-                  <Route
-                    path="/"
-                    element={<Dashboard isDarkMode={isDarkMode} />}
-                  />
-                  <Route
-                    path="/notifications"
-                    element={<Notifications isDarkMode={isDarkMode} />}
-                  />
-                  <Route
-                    path="/grades"
-                    element={<StudentGrades isDarkMode={isDarkMode} />}
-                  />
-                  <Route
-                    path="/assignments"
-                    element={<Assignment isDarkMode={isDarkMode} />}
-                  />
-                  <Route
-                    path="/student-assignment"
-                    element={<StudentAssignment isDarkMode={isDarkMode} />}
-                  />
-                  <Route
-                    path="/student-grades"
-                    element={<ShowStudentGrades isDarkMode={isDarkMode} />}
-                  />
-                </Routes>
-              </main>
-              {/* Footer (Scrollable) */}
-              <Footer />
-            </div>
+          {/* Main Content Wrapper (Scrollable) */}
+          <div className="flex flex-col flex-grow overflow-y-auto mt-[136px] md:mt-[76px]">
+            <main
+              className={`flex-grow p-4 lg:p-8 ${
+                isDarkMode
+                  ? "bg-gray-900 text-white"
+                  : "bg-gray-100 text-gray-700"
+              }`}
+            >
+              <Routes>
+                <Route
+                  path="/"
+                  element={<Dashboard isDarkMode={isDarkMode} />}
+                />
+                <Route
+                  path="/notifications"
+                  element={<Notifications isDarkMode={isDarkMode} />}
+                />
+                <Route
+                  path="/grades/:id"
+                  element={<StudentGrades isDarkMode={isDarkMode} />}
+                />
+                <Route
+                  path="/assignments"
+                  element={<Assignment isDarkMode={isDarkMode} />}
+                />
+                <Route
+                  path="/student-assignment"
+                  element={<StudentAssignment isDarkMode={isDarkMode} />}
+                />
+                <Route
+                  path="/student-grades"
+                  element={<ShowStudentGrades isDarkMode={isDarkMode} />}
+                />
+              </Routes>
+            </main>
+            {/* Footer (Scrollable) */}
+            <Footer />
           </div>
         </div>
-      </NotificationsProvider>
+      </div>
     </div>
   );
 };
