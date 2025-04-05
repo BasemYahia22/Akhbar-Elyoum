da82b15bbbb25daf77ff92e5b9370e480e7cec49
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 19:35:20 +0200

    feat: Add logout route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import ProfessorDashboard from "./pages/professor/ProfessorDashboard";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import LogoutHandler from "./components/LogoutHandler";
 
 function App() {
   return (
@@ -14,20 +15,17 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/login" element={<Login />} />
-
+        <Route path="/logout" element={<LogoutHandler />} />
         {/* Protected Routes */}
-        <Route element={<ProtectedRoute requiredRole="student" />}>
+        <Route element={<ProtectedRoute requiredRole="Student" />}>
           <Route path="/student/*" element={<StudentDashboard />} />
         </Route>
-
-        <Route element={<ProtectedRoute requiredRole="professor" />}>
+        <Route element={<ProtectedRoute requiredRole="Professor" />}>
           <Route path="/professor/*" element={<ProfessorDashboard />} />
         </Route>
-
-        <Route element={<ProtectedRoute requiredRole="admin" />}>
+        <Route element={<ProtectedRoute requiredRole="Admin" />}>
           <Route path="/admin/*" element={<AdminDashboard />} />
         </Route>
-
         {/* Default Redirect */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
