47bda30f03d05476ae803316999cd4a2f2ee59bf
BasemYahia22 <basemyahiagithup@gmail.com>
2025-04-05 17:27:03 +0200

    feat: fetch data from backend API in Courses

--- src/pages/student/Courses.jsx
+++ src/pages/student/Courses.jsx
@@ -1,93 +1,49 @@
 import UserDataSection from "../../components/UserDataSection";
 import CourseCardWithGrades from "../../components/CourseCardWithGrades";
-import ReviewGradesModal from "../../components/ReviewGradesModal";
-import { useState } from "react";
-
-const CoursesData = [
-  {
-    id: 1,
-    name: "Advanced Mathematics",
-    instructor: "Dr. John Doe",
-    prerequisite: "CS350",
-    midtermGrade: 92,
-    yearworkGrade: 88,
-    finalGrade: 95,
-    assignments: [
-      { name: "Assignment 1", grade: 95 },
-      { name: "Assignment 2", grade: 87 },
-      { name: "Assignment 3", grade: 90 },
-    ],
-  },
-  {
-    id: 2,
-    name: "Advanced Mathematics",
-    instructor: "Dr. John Doe",
-    prerequisite: "CS350",
-    midtermGrade: 92,
-    yearworkGrade: 88,
-    finalGrade: 95,
-    assignments: [
-      { name: "Assignment 1", grade: 95 },
-      { name: "Assignment 2", grade: 87 },
-      { name: "Assignment 3", grade: 90 },
-    ],
-  },
-  {
-    id: 3,
-    name: "Advanced Mathematics",
-    instructor: "Dr. John Doe",
-    prerequisite: "CS350",
-    midtermGrade: 92,
-    yearworkGrade: 88,
-    finalGrade: 95,
-    assignments: [
-      { name: "Assignment 1", grade: 95 },
-      { name: "Assignment 2", grade: 87 },
-      { name: "Assignment 3", grade: 90 },
-    ],
-  },
-  {
-    id: 4,
-    name: "Advanced Mathematics",
-    instructor: "Dr. John Doe",
-    prerequisite: "CS350",
-    midtermGrade: 92,
-    yearworkGrade: 88,
-    finalGrade: 95,
-    assignments: [
-      { name: "Assignment 1", grade: 95 },
-      { name: "Assignment 2", grade: 87 },
-      { name: "Assignment 3", grade: 90 },
-    ],
-  },
-  {
-    id: 5,
-    name: "Advanced Mathematics",
-    instructor: "Dr. John Doe",
-    prerequisite: "CS350",
-    midtermGrade: 92,
-    yearworkGrade: 88,
-    finalGrade: 95,
-    assignments: [
-      { name: "Assignment 1", grade: 95 },
-      { name: "Assignment 2", grade: 87 },
-      { name: "Assignment 3", grade: 90 },
-    ],
-  },
-];
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStudentCourses } from "../../redux/slices/fetchStudentCoursesSlice";
+import Message from "../../components/Message"; // Import the Message component
+import ReviewGradesModal from "./ReviewGradesModal";
+import StatusMessage from "../../components/StatusMessage";
 
 const Courses = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector(
+    (state) => state.fetchStudentCourses
+  );
+
+  useEffect(() => {
+    if (!data) {
+      dispatch(fetchStudentCourses());
+    }
+  }, [dispatch, data]);
+
+  const showMessage = (msg, type) => {
+    setMessage(msg);
+    setMessageType(type);
+  };
+
+  const hideMessage = () => setMessage("");
+
+  if (loading || error) {
+    return <StatusMessage loading={loading} error={error} />;
+  }
 
   return (
     <section>
       <UserDataSection />
+      {/* CourseCardWithGrades */}
       <div className="grid gap-5 mt-10 lg:grid-cols-3 md:grid-cols-2">
-        {CoursesData.map((course) => {
-          return <CourseCardWithGrades course={course} key={course.id} />;
+        {data?.result.courses.map((course) => {
+          return (
+            <CourseCardWithGrades course={course} key={course.course_id} />
+          );
         })}
       </div>
-
       {/* Button to Open Modal */}
       <div className="mt-6 text-center">
         <button
@@ -97,12 +53,14 @@ const Courses = () => {
           Review Grades
         </button>
       </div>
-
       {/* Review Grades Modal */}
       <ReviewGradesModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
+        showMessage={showMessage} // Pass the showMessage function
       />
+      {/* Display the message in the Courses component */}
+      <Message message={message} type={messageType} onClose={hideMessage} />
     </section>
   );
 };
